577b33c5a3b76478e38ef6e412ed33b87537ecc7
Antonio de Jesus Hernandez Piña <tonyhdezpi@gmail.com>
2022-08-10 18:08:48 -0500

    Description to the method 'getScore' is added

--- models/BowlingGame.js
+++ models/BowlingGame.js
@@ -24,12 +24,15 @@ class BowlingGame{
   // This method makes the score for every frame
   getScore () {
     for (let i = 0; i < this.game.length; i++) {
+      // if the firs roll, give us a 10, then its a strike, so the strike bonus is apply
       if (this.game[i][0] === 10) {
         this.score += (this.game[i + 1][0] + this.game[i + 1][1])
       }
+      // if the second roll give us a 10, then is a spare, so spare bonus is apply
       if (this.game[i][0] === 0 && this.game[i][1] === 10) {
         this.score += this.game[i + 1][0]
       }
+      // if none of this conditions are apply, then just adds the first and second roll to score.
       this.score += (this.game[i][0] + this.game[i][1])
     }
     return this.score
@@ -44,14 +47,6 @@ class BowlingGame{
 
   getGame () {
     return this.game
-  }
-
-  bonusCalculator () {
-
-  }
-
-  addToGame () {
-
   }
 }
 module.exports = BowlingGame
